b275d45ef762664047981bd8b2d81ec30b9eb600
DavidWaza <wazabanga00@gmail.com>
2022-04-15 14:49:42 +0100

    code refactoring

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import "./App.css";
-import SearchApi from "./seachApi";
+import axios from "axios";
+import Autocomplete from "./Autocomplete";
+import { useState, useEffect } from "react";
 
+const url = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBS0jDQGP-Gw3c32AS0487MevOQIB7gqkk&libraries=places&callback=initMap&solution_channel=GMP_QB_addressselection_v1_cABC"
 function App() {
+  const [data, setData] = useState([])
+
+  const Product = () => {
+    useEffect(()=> {
+      axios.get(url).then((res) => {
+        setData(res.data)
+      })
+    }, [])
+    if(!data) return null;
+  }
+
+
   return (
     <div className="App">
       <div>
         <h1>Search Automedys</h1>
-        <SearchApi />
+       <Autocomplete
+        suggestions={[
+          "Alligator",
+          "Bask",
+          "Crocodilian",
+          "Death Roll",
+          "Eggs",
+          "Jaws",
+          "Reptile",
+          "Solitary",
+          "Tail",
+          "Wetlands"
+        ]}
+
+      />
       </div>
     </div>
   );
---
+++ src/Autocomplete.js
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import "./App.css";
+
+const Autocomplete = ({ suggestions }) => {
+  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+  const [activeSuggestionIndex, setActiveSuggestionIndex] = useState(0);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [input, setInput] = useState("");
+
+  const onChange = (e) => {
+    const userInput = e.target.value;
+    
+// Filter our suggestions that does not contain the user's input
+    const unLinked = suggestions.filter(
+      (suggestion) =>
+        suggestion.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+    );
+    setInput(e.target.value);
+    setFilteredSuggestions(unLinked);
+    setActiveSuggestionIndex(0);
+    setShowSuggestions(true);
+  };
+
+  // when a user clicks for suggestions
+  const onClick = (e) => {
+    setInput(e.target.innerText);
+    setFilteredSuggestions([]);
+    setActiveSuggestionIndex(0);
+    setShowSuggestions(false);
+  }
+const SuggestionsListComponent = () => {
+  return filteredSuggestions.length ? (
+    <ul className="suggestion">
+      {filteredSuggestions.map((suggestion, index) => {
+        let className;
+
+        if(index === activeSuggestionIndex){
+          className = "active-suggestion"
+        }
+        return (
+          <li key={suggestions} onClick={onClick} className={className} >
+            {suggestion}
+          </li>
+        )
+      })}
+    </ul>
+  ) : (
+    <div className="no-suggestion">
+      <em>No suggestion</em>
+    </div>
+  )
+}
+  return (
+    <div className="App">
+      <>
+        <input
+          type="text"
+          className="suggestion-active"
+          placeholder="input address"
+          value={input}
+          onChange={onChange}
+        />
+        {showSuggestions && input && <SuggestionsListComponent />}
+      </>
+    </div>
+  );
+};
+
+export default Autocomplete;
