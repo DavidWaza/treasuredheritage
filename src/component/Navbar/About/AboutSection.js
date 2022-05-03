3fe5f3586a5d178e25c9f2be689a5c56debd3001
DavidWaza <wazabanga00@gmail.com>
2022-05-03 11:39:03 +0100

    a little tweak in various components

--- src/component/Navbar/About/AboutSection.js
+++ src/component/Navbar/About/AboutSection.js
@@ -1,21 +1,19 @@
 import "./AboutSection.css";
 import { Container, Row, Col } from "react-bootstrap";
+import DiamondSmall from "../../../images/diamondxs.png";
+import VisionSmall from "../../../images/visionxs.png";
 
 const AboutSection = () => {
   const AboutHeader = { text: "what we do" };
-  const AboutDress = { text: "dressing code ths" };
   const AboutSubHeader = [
-    "Treasured Heritage School (THS) is a Christian Montessori  School that offers educational services from Creche to Grade levels. We are poised to build a total child that is spiritually, morally and academically sound, exuding self  confidence that can make the child compete favourably with",
-  ];
-  const AboutSubDress = [
-    " In THS, we place high premium on proper dressing. Pupils must ALWAYS appear neatly dressed with their white socks, black shoes and trimmed nails. Boys and girls must have their hair properly done. No use of hair extension please. Boys must also have their hair cut low. Any apparel that is not in tandem with the school’s recommendation shall be confiscated.",
+    "Treasured Heritage School (THS) is a Christian Montessori  School that offers educational services from Creche to Grade levels. We are poised to build a total child that is spiritually, morally and academically sound, exuding self  confidence that can make the child compete favourably with.",
   ];
   return (
     <div>
       <section>
         <Container>
           <Row>
-            <Col className="text_section" style={{paddingTop:25}}>
+            <Col className="text_section" style={{ paddingTop: 25 }}>
               <h3>ABOUT THS</h3>
               <a href="#About">
                 <p style={{ marginBottom: 20 }}>Know More > </p>
@@ -29,14 +27,45 @@ const AboutSection = () => {
               <div className="image_abt"></div>
             </Col>
             <Col sm={6}>
-              <div className="ui piled segment">
-                <h4 className="ui header"> {AboutHeader.text} </h4>
-                <p style={{fontSize:"1.1357rem"}}>{AboutSubHeader}</p>
+              <div>
+                <h4 className="ui header "> {AboutHeader.text} </h4>
+                <p style={{ fontSize: "1.1357rem" }} className="subHeader">
+                  {AboutSubHeader}
+                </p>
               </div>
-              <div className="ui piled segment">
-                <h4 className="ui header"> {AboutDress.text} </h4>
-                <p style={{fontSize:"1.1357rem"}}>{AboutSubDress}</p>
-              </div>
+              <Row style={{ paddingLeft: 30, marginTop: 20 }}>
+                <Col sm={6}>
+                  <img src={DiamondSmall} className="small_png" alt="" />
+                  <div>
+                    <div className="header_head">
+                      <header>Our Mission</header>
+                    </div>
+                    <p>
+                       Provide a sound Montessori system of education required{" "}
+                      <br />
+                      for the acquisition of relevant knowledge and skills.
+                      <br />
+                      <a href="#MisVis"><div className="ui button" style={{marginTop:15}}>Show more</div></a>
+                      <br />
+                    </p>
+                  </div>
+                </Col>
+                <Col sm={6}>
+                  <img src={VisionSmall} className="small_png" alt="" />
+                  <div>
+                    <div className="header_head">
+                      <header>Our Vision</header>
+                    </div>
+                    <p>
+                      To be among the foremost providers of all-round
+                      qualitative education in Nigeria.
+                      <br />
+                      <div className="ui button" style={{marginTop:35}}>Show more</div>
+                      <br />
+                      </p>
+                  </div>
+                </Col>
+              </Row>
             </Col>
           </Row>
         </Container>
